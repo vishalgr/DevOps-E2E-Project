9f0ad1ef3adc9e16924586859f02c29376ac7380
Manikanta Venkateshappa <Manikanta.Venkateshappa@philips.com>
2020-04-13 21:24:53 +0200

    Example Hello world added

---
+++ Examples/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelperMethods helperMethods = new HelperMethods();
+            var stringToPrint = helperMethods.ReturnTheInput("Hello World !!!");
+            Console.WriteLine(stringToPrint);
+        }
+    }
+}
