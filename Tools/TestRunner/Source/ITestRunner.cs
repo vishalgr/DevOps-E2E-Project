9c6ee49d878b055e0596aa28359c135a78835545
subbudatta <52562796+subbudatta@users.noreply.github.com>
2020-05-05 13:35:00 +0530

    Test Runner And CSV Converter Added

---
+++ Tools/TestRunner/Source/ITestRunner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.TestRunner
+{
+    interface ITestRunner {
+        int Execute(string testAssembly, DirectoryInfo outPutDirectory);
+
+        List<string> FindTestAssemblies(DirectoryInfo searchDirectory);
+    }
+}
---
+++ Tools/TestRunner/Source/MSTest/TestRunner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOps.TestRunner.MSTest
+{
+    class TestRunner : ITestRunner
+    {
+
+        public int Execute(string testAssembly, DirectoryInfo outPutDirectory) {
+            throw new NotImplementedException();
+        }
+
+        public List<string> FindTestAssemblies(DirectoryInfo searchDirectory) {
+            throw new NotImplementedException();
+        }
+
+    }
+}
