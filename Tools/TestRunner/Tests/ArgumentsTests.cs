3cd48dede245fc618b3fa2953dea6913ce26f318
Manikanta Venkateshappa <Manikanta.Venkateshappa@philips.com>
2020-05-08 11:32:56 +0200

    Binaries and obj shall not be committed.

--- Tools/TestRunner/Tests/ArgumentsTests.cs
+++ Tools/TestRunner/Tests/ArgumentsTests.cs
@@ -17,8 +17,31 @@ namespace DevOps.Tests.TestRunner
         //string myString = "DevOps.TestRunner --Executor  D:\\Applications\\nunit-console\\nunit3-console.exe  --TestFramework  NUnit --AssemlbyDirectory D:\\Views\\Testing\\Testing\\bin\\Debug --OutputDirectory  D:\\DevopsTestResults";
         //string[] myString.Cast<char>().Cast<string>().ToArray();
         private DevOps.TestRunner.Arguments TestingObject;
-        private static string[] InputToTest =  { "DevOps.TestRunner","--Executor"," D:\\Applications\\nunit-console\\nunit3-console.exe"," --TestFramework"," NUnit"," --AssemlbyDirectory", "D:\\Views\\Testing\\Testing\\bin\\Debug", "--OutputDirectory"," D:\\DevopsTestResults" };
-        private static string[] InputToTest2 = { "DevOps.TestRunner", "--Executor", " D:\\Applications\\nunit3-console.exe", " --TestFramework", " NUnit", " --AssemlbyDirectory", "D:\\Views\\Testing\\Testing\\bin\\Debug", "--OutputDirectory", " D:\\DevopsTestResults" };
+
+        private static string[] InputToTest = {
+            "DevOps.TestRunner",
+            "--Executor",
+            " D:\\Applications\\nunit-console\\nunit3-console.exe",
+            " --TestFramework",
+            " NUnit",
+            " --AssemlbyDirectory",
+            "D:\\Views\\Testing\\Testing\\bin\\Debug",
+            "--OutputDirectory",
+            " D:\\DevopsTestResults"
+        };
+
+        private static string[] InputToTest2 = {
+            "DevOps.TestRunner",
+            "--Executor",
+            " D:\\Applications\\nunit3-console.exe",
+            " --TestFramework",
+            " NUnit",
+            " --AssemlbyDirectory",
+            "D:\\Views\\Testing\\Testing\\bin\\Debug",
+            "--OutputDirectory",
+            " D:\\DevopsTestResults"
+        };
+
         [SetUp]
         public void TestSetup()
         {
@@ -27,7 +50,6 @@ namespace DevOps.Tests.TestRunner
             Console.WriteLine(InputToTest2.GetType());
             TestingObject = new DevOps.TestRunner.Arguments();
         }
-
        
         [Test,TestCaseSource("InputToTest")]
         public void TestWillResturnTrueIfAlltheInputIsCorrect(string[] input)
@@ -44,10 +66,6 @@ namespace DevOps.Tests.TestRunner
             bool returnValue = TestingObject.Parse(input);
             Assert.AreEqual(true, returnValue, "Expected different input");
         }
-
-
-
-
 
         [TearDown]
         public void TestTearDown()
