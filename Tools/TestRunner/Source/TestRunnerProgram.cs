5a3bc5f57cff895a3282a13b1ba31f27e2837d14
Subramanya Datta S <subramanya360@gmail.com>
2020-05-13 15:14:50 +0530

    Error message is displayed if the directory is not found

--- Tools/TestRunner/Source/TestRunnerProgram.cs
+++ Tools/TestRunner/Source/TestRunnerProgram.cs
@@ -7,12 +7,13 @@ using System.Threading.Tasks;
 namespace DevOps.TestRunner
 {
     class TestRunnerProgram {
-        static int Main(string[] args) 
+        static int Main(string[] args)
         {
             int exitCode = 0;
             // Parse arguments
             var arguments = new Arguments();
-            if (! arguments.Parse(args)) {
+            if (!arguments.Parse(args))
+            {
                 Console.WriteLine("Parsing input arguments failed: " + arguments.ErrorMessage);
                 Console.WriteLine(Arguments.Usage);
                 return -1;
@@ -21,15 +22,27 @@ namespace DevOps.TestRunner
 
             ITestRunner testRunner = GetTestRunner(arguments);
             var testAssemblies = testRunner.FindTestAssemblies(arguments.AssemblyDirectory);
-            foreach (var testAssembly in testAssemblies) {
-                int returnValue = testRunner.Execute(testAssembly, arguments.OutputDirectory);
-                if (returnValue < 0) {
-                    exitCode = -1;
+            if (testAssemblies.Count > 0)
+            {
+                foreach (var testAssembly in testAssemblies)
+                {
+                    int returnValue = testRunner.Execute(testAssembly, arguments.OutputDirectory);
+                    if (returnValue < 0)
+                    {
+                        exitCode = -1;
+                    }
                 }
+                Console.WriteLine("TestRunner executed successfully");
+                return exitCode;
             }
-            Console.WriteLine("TestRunner executed successfully");
-            return exitCode;
+            else
+            {
+                Console.WriteLine("Couldnt Find The Tests,Please Give The Correct Directory");
+                exitCode = -1;
+                return exitCode;
+            }
         }
+        
 
         private static ITestRunner GetTestRunner(Arguments arguments) {
             ITestRunner testRunner = null;
