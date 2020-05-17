1f914b54a2b234719d3d0b4fa5b1d1e47addc735
poornimayo <55318034+poornimayo@users.noreply.github.com>
2020-05-17 18:11:31 +0530

    Update LoginTest.cs

--- Tools/website/Tests/LoginTest.cs
+++ Tools/website/Tests/LoginTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,8 @@ using NUnit.Framework;
 namespace Tests
 {
     [TestFixture]
-    public class LoginTest {
+    public class LoginTest
+    {
         IWebDriver driver;
         string url = "http://localhost:56666/";
 
@@ -19,9 +20,10 @@ namespace Tests
             new object[] { "UserNotExists", "admin" }, // incorrect username, correct password
             new object[] { "UserNotExists", "invalidPassword" }, // incorrect username, incorrect password
         };
-        
+
         [SetUp]
-        public void Initialization() {
+        public void Initialization()
+        {
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl(url);
             driver.Manage().Window.Maximize();
@@ -29,21 +31,28 @@ namespace Tests
 
         //Verify if a user will be able to login with a valid username and valid password.  
         [Test]
-        public void TestThatLoginSucceedsWithValidCredentials() {
-            IWebElement username = driver.FindElement(By.Id("Username"));
-            username.SendKeys("admin");
-            IWebElement password = driver.FindElement(By.Id("Password"));
-            password.SendKeys("admin");
-            System.Threading.Thread.Sleep(1000);
+        public void TestThatLoginSucceedsWithValidCredentials()
+        {
+            var userName = "admin";
+            var passWord = "admin";
+            new System.Threading.ManualResetEvent(false).WaitOne(1000);
+            Assert.AreSame("admin", userName, "Invalid username");
+            Assert.AreSame("admin", passWord, "Invalid password");
+            SendKeysToElement("Username", userName);
+            SendKeysToElement("Password", passWord);
+            new System.Threading.ManualResetEvent(false).WaitOne(100);
             driver.FindElement(By.Id("btnLogin")).Click();
-            Console.WriteLine("successfull login");
-            System.Threading.Thread.Sleep(1000);
             driver.Navigate().Forward();
+            
+            Console.WriteLine("successfull login");
+            
+            
         }
 
         //Verify if a user cannot login with a valid username and an invalid password.
         [Test, TestCaseSource("invalidCredentials")]
-        public void TestThatLoginFailsWithInValidCredentials(object[] credentials) {
+        public void TestThatLoginFailsWithInValidCredentials(object[] credentials)
+        {
             var userName = credentials[0].ToString();
             var passWord = credentials[1].ToString();
             Assert.IsNotEmpty(userName, "Expected username value");
@@ -51,43 +60,29 @@ namespace Tests
 
             SendKeysToElement("Username", userName);
             SendKeysToElement("Password", passWord);
-            System.Threading.Thread.Sleep(1000);
+            new System.Threading.ManualResetEvent(false).WaitOne(100);
             driver.FindElement(By.Id("btnLogin")).Click();
-            
+
             Console.WriteLine("wrong credentials");
-            System.Threading.Thread.Sleep(1000);
+            new System.Threading.ManualResetEvent(false).WaitOne(100);
             driver.Navigate().Back();
-            System.Threading.Thread.Sleep(1000);
+            new System.Threading.ManualResetEvent(false).WaitOne(1000);
             driver.Navigate().GoToUrl(url);
         }
 
-        //Verify the login page for both, when the field is blank and Submit button is clicked.
-        // TODO: I did not understand what this method is testing.
-        [Test]
-        public void spacebuttoncredentials() {
-            IWebElement username = driver.FindElement(By.Id("Username"));
-            username.SendKeys("");
-            IWebElement password = driver.FindElement(By.Id("Password"));
-            password.SendKeys("");
-            System.Threading.Thread.Sleep(1000);
-            driver.FindElement(By.Id("btnLogin")).Click();
-
-            Console.WriteLine("Userid or password could not be Empty.");
-            System.Threading.Thread.Sleep(1000);
-            driver.Navigate().Back();
-            System.Threading.Thread.Sleep(1000);
-            driver.Navigate().GoToUrl(url);
-        }
+        
 
         // Helper functions
-        private void SendKeysToElement(string webElementId, string textToEnter) {
+        private void SendKeysToElement(string webElementId, string textToEnter)
+        {
             var webElement = driver.FindElement(By.Id(webElementId));
             Assert.IsNotNull(webElement, "No element found with the id: " + webElement);
             webElement.SendKeys(textToEnter);
         }
 
         [TearDown]
-        public void cleanup() {
+        public void cleanup()
+        {
             driver.Dispose();
             Console.WriteLine("Browser window is closed");
             driver.Quit();
