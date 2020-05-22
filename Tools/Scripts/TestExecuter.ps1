3e781dd93a5988552e43384990d1f6a327365ea6
poornimayo <poornimayo@gmail.com>
2020-05-22 21:13:19 +0530

    Powershell script

--- Tools/Scripts/TestExecuter.ps1
+++ Tools/Scripts/TestExecuter.ps1
@@ -1,98 +1,148 @@
-﻿
-[CmdletBinding()]
+﻿[CmdletBinding()]
+
 Param (
+
 # Executor to execute the tests
-[Parameter(Mandatory=$true)]
+
+    [Parameter(Mandatory=$true)]
+
 $Executor,
 
 # Add some description here
-[Parameter(Mandatory=$true)]
+
+    [Parameter(Mandatory=$true)]
+
 $TestFramework,
 
 # Add some description here
-[Parameter(Mandatory=$true)]
+
+    [Parameter(Mandatory=$true)]
+
 $AssemblyDirectory,
 
 # Add some description here
-[Parameter(Mandatory=$true)]
+
+    [Parameter(Mandatory=$true)]
+
 $OutputDirectory
+
 )
 
+$MyDir = Get-Location
+
 # Variables
-$testRunnerExe = "..\TestRunner\Output\DevOps.TestRunner.exe"
-$csvConverterExe = "..\TestRunner\Output\DevOps.CSVConverter.exe"
+    $ReturnVal = -1
+    $Count = 1
+    $testRunnerExe = Join-Path -Path "$MyDir" -ChildPath "\TestRunner\Output\DevOps.TestRunner.exe"
+    $csvConverterExe = Join-Path -Path "$MyDir" -ChildPath "\CSVConverter\Output\DevOps.CSVConverter.exe"
 
 # Functions
-Function Log($message) {
-    Write-Host ($(get-date -Format "yyyymmdd-HHMMss: ") + "$message")
-}
 
-# End of functions Functions
-# TODO: 
-# 1. Help message :referer to https://stackoverflow.com/questions/5237723/how-do-i-get-help-messages-to-appear-for-my-powershell-script-parameters
-# 2. Start-Transcript: use this
-# 3. 
+    Function Log($message) 
 
-try {
+    {
 
-    Log "Script execution started"
-    # Check pre-requisites
-    If(-Not $testRunnerExe ) {
-        throw "Test runner does not exists: $testRunnerExe"
+        Write-Host ($(get-date -Format "yyyymmdd-HHMMss: ") + "$message")
+
     }
-    If(-Not $csvConverterExe ) {
-        throw "CSV converter does not exists: $csvConverterExe"
-    }
+
+    function toolcall($program,$PassedArguments)
+     {
     
-    # Execute
-    # TODO: Invoke below within a process and check the exit code.
-    # Invoke TestRunner
-    $testRunnerArguments = @()
-    $testRunnerArguments += "--Executor `"$Executor`" "
-    $testRunnerArguments += "--TestFramework `"$TestFramework`" "
-    $testRunnerArguments += "--AssemlbyDirectory `"$AssemblyDirectory`" "
-    $testRunnerArguments += "--OutputDirectory `"$OutputDirectory`" "
-    &$testRunnerExe $testRunnerArguments
+        $ProcessInfo= New-Object System.Diagnostics.ProcessStartInfo
+        $ProcessInfo.FileName = $program
+        $ProcessInfo.RedirectStandardError = $true
+        $ProcessInfo.RedirectStandardOutput = $true
+        $ProcessInfo.UseShellExecute = $false
+        $ProcessInfo.Arguments = $PassedArguments
+        $ProcessObj = New-Object System.Diagnostics.Process
+        $ProcessObj.StartInfo = $processInfo
+        $ProcessObj.Start() | Out-Null
+        $ProcessObj.WaitForExit()
+        $stdout = $ProcessObj.StandardOutput.ReadToEnd()
+        $stderr = $ProcessObj.StandardError.ReadToEnd()
+        Write-Host "stdout: $stdout"
+        Write-Host "stderr: $stderr"
+        Write-Host "exit code: " + $ProcessObj.ExitCode
+        
+        return $ProcessObj.ExitCode 
+    }
 
-    # TODO: if exit code states failed, then no need to execute next command.
+    try 
 
-    # Invoke CSVConverter
-    # Refer to the test runner code (available above)
+    {
+
+        Log "Script execution started"
+
+        # Check pre-requisites
+
+            If(-Not $testRunnerExe )
+         
+            {
+            
+                throw "Test runner does not exists: $testRunnerExe"
+            }
+
+            If(-Not $csvConverterExe ) 
+
+            {
+
+               throw "CSV converter does not exists: $csvConverterExe"
+            }
+
+        # Execute
+
+        # TODO: Invoke below within a process and check the exit code.
+
+        # Invoke TestRunner
+
+        $testRunnerArguments = @()
+
+        $testRunnerArguments +="--Executor `"$Executor`" "
+
+        $testRunnerArguments +="--TestFramework `"$TestFramework`" "
+
+        $testRunnerArguments +="--AssemblyDirectory `"$AssemblyDirectory`" "
+
+        $testRunnerArguments +="--OutputDirectory `"$OutputDirectory`" "
+
+        #&$testRunnerExe $testRunnerArguments
+        $ReturnVal=toolcall -program "$testRunnerExe" -PassedArguments "$testRunnerArguments" 
 
 
-    # check if everything is fine
-    Log "Script execution completed successfully"
 
-} catch {
-    Log "Script execution failed"
-    Write-Host "$_" -BackgroundColor Red
-} finally {
-    # Reserved place to perform cleanup activities.
-}
+        if($ReturnVal -eq 0)
+        {
+        toolcall -program "$csvConverterExe" -PassedArguments "$CSVConverterArguments" 
+        }
 
+        #CSVarguments
 
+        $CSVConverterArguments = @()
 
+        $CSVConverterArguments += "--XmlFileDirectory `"$OutputDirectory`" "
 
-#This returns current working directory if you wish to use!!
+        $CSVConverterArguments += "--OutputDirectory `"$OutputDirectory`" "
+        
+        
+        
 
- 
+        Log "Script execution completed successfully"
 
-#$ScriptRoot = Split-Path -Parent $PSCommandPath
+    } 
 
-#$exe1  = "$ScriptRoot\TestRunner\Output\DevOps.TestRunner.exe"
+    catch 
 
-#$exe2 = "$ScriptRoot\CSVConverter\CSVConverter\bin\Debug\CSVConverter.exe"
+    {
 
- <#
+        Log "Script execution failed"
 
-$exe1  = '.\TestRunner\Output\DevOps.TestRunner.exe'
+        Write-Host "$_" -BackgroundColor Red
 
-$exe2 = ".\CSVConverter\CSVConverter\bin\Debug\CSVConverter.exe"
+    }
 
+    finally 
+    {
 
-& $exe1 --Executor $Executor --TestFramework $TestFramework --OutputDirectory $OutputDirectory --AssemlbyDirectory $AssemblyDirectory
-
- 
-
-& $exe2 --AssemlbyDirectory $OutputDirectory --OutputDirectory $OutputDirectory
-#>
+     # Reserved place to perform cleanup activities.
+    }
