228a25088e52618be1f63d7f28c5f99410e96180
VishalGR <42291173+vishalgr@users.noreply.github.com>
2020-05-31 14:28:22 +0530

    Add files via upload

---
+++ LoginApplication/Source/Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginApplication
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string username = TextBox1.Text;
+            string password = TextBox2.Text;
+
+            if (username == "admin" & password == "admin")
+            {
+                Response.Redirect("Homepage.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Wrong Credentials');</script>");
+            }
+        }
+    }
+}
---
+++ LoginApplication/Test/LoginTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+namespace DevOps.Test.LoginApplication
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        IWebDriver driver;
+        string url = "https://localhost:44353/";
+
+        private static List<object> invalidCredentials = new List<object>() {
+            new object[] { "admin", "invalidPassword" }, // correct username, incorrect password
+            new object[] { "UserNotExists", "admin" }, // incorrect username, correct password
+            new object[] { "UserNotExists", "invalidPassword" }, // incorrect username, incorrect password
+        };
+
+        [SetUp]
+        public void Initialization()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        }
+
+        //Verify if a user will be able to login with a valid username and valid password.  
+        [Test]
+        public void TestThatLoginSucceedsWithValidCredentials()
+        {
+            var userName = "admin";
+            var passWord = "admin";
+            Assert.AreSame("admin", userName, "Invalid username");
+            Assert.AreSame("admin", passWord, "Invalid password");
+            SendKeysToElement("ContentPlaceHolder1_TextBox1", userName);
+            SendKeysToElement("ContentPlaceHolder1_TextBox2", passWord);
+            driver.FindElement(By.Id("ContentPlaceHolder1_Button1")).Click();
+            driver.Navigate().Forward();
+
+            Console.WriteLine("successfull login");
+
+
+        }
+
+        //Verify if a user cannot login with a valid username and an invalid password.
+        [Test, TestCaseSource("invalidCredentials")]
+        public void TestThatLoginFailsWithInValidCredentials(object[] credentials)
+        {
+            var userName = credentials[0].ToString();
+            var passWord = credentials[1].ToString();
+            Assert.IsNotEmpty(userName, "Expected username value");
+            Assert.IsNotEmpty(passWord, "Expected passWord value");
+
+            SendKeysToElement("ContentPlaceHolder1_TextBox1", userName);
+            SendKeysToElement("ContentPlaceHolder1_TextBox2", passWord);
+            driver.FindElement(By.Id("ContentPlaceHolder1_Button1")).Click();
+            Console.WriteLine("wrong credentials");       
+        }
+
+
+
+        // Helper functions
+        private void SendKeysToElement(string webElementId, string textToEnter)
+        {
+            var webElement = driver.FindElement(By.Id(webElementId));
+            Assert.IsNotNull(webElement, "No element found with the id: " + webElement);
+            webElement.SendKeys(textToEnter);
+        }
+
+        [TearDown]
+        public void cleanup()
+        {
+            driver.Dispose();
+            Console.WriteLine("Browser window is closed");
+            driver.Quit();
+        }
+    }
+}
