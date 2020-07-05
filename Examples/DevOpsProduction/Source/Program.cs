15c7f80a51bec9b8c6acb9c608bed17ff67d3f76
Manikanta Venkateshappa <Manikanta.Venkateshappa@philips.com>
2020-07-05 11:33:11 +0200

    Corrected the namespace

--- Examples/DevOpsProduction/Source/Program.cs
+++ Examples/DevOpsProduction/Source/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace DevOps.Tests.Production
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace DevOps.Production {
+    class Program {
+        static void Main(string[] args) {
             int num1 = 10;
             int num2 = 14;
             Helper a = new Helper();
@@ -16,19 +13,15 @@ namespace DevOps.Tests.Production
             Console.WriteLine(q);
         }
     }
-    public class Helper
-    {
-        public int add(int a, int b)
-        {
+    public class Helper {
+        public int add(int a, int b) {
             int x = a + b;
             return x;
         }
     }
 
-    public class randomnumbergenerator
-    {
-        public int RandomNumber()
-        {
+    public class randomnumbergenerator {
+        public int RandomNumber() {
             System.Random random = new System.Random();
             int num = random.Next(1, 50);
             return num;
--- Examples/DevOpsProduction/Tests/Tester.cs
+++ Examples/DevOpsProduction/Tests/Tester.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DevOps.Tests.Production;
+using DevOps.Production;
 
-namespace DevOps.Tests.ProductionTests
+namespace DevOps.Tests.Production
 {
     [TestFixture]
     public class Tester
